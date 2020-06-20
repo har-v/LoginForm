813b46d7643c38b0c938006c0b20672f8f43d527
Alice Harvey <66881641+har-v@users.noreply.github.com>
2020-06-20 05:49:11 +0100

    Add files via upload

---
+++ Old Design/userProfile/time.js
@@ -0,0 +1,40 @@
+var date = new Date();
+var monthsArr = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'DEC'];
+var month = monthsArr[date.getMonth()];
+var year = date.getFullYear();
+var day = date.getDate();
+
+document.getElementById('day').textContent = day;
+document.getElementById('month').textContent = month;
+document.getElementById('year').textContent = year;
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+
+    m = checkTime(m);
+
+    document.getElementById('time').innerHTML = h + ":" + m ;
+    t = setTimeout(function() {
+        startTime()
+    }, 500);
+}
+startTime();
+
+
+
+
+
+
+
+
+
+
